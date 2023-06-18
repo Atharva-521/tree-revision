23b5b819b161c5632070dd02020c4cbe7ae8efa7
Atharva <collageandwrok@gmail.com>
2023-06-18 14:04:08 +0530

    vertical traversal added previosly not added mistake in commit

--- verticaltraversal.cpp
+++ verticaltraversal.cpp
@@ -7,34 +7,69 @@
 
 using namespace std;
    
-map<int,vector<int> > mapping(node*root,int &hd)
-{
-    map<int,vector<int> > mapped;
-    
-    //base case 
-    if(root == NULL)
-    {
-        return mapped;
-    }
 
-    mapped[hd].push_back(root->data);
-    mapping(root->left,hd-1);
-    mapping(root->right,hd+1);
-
-    return mapped;
-
-
-}
 
 vector<int> vertical(node* root)
 {
-    mapping(root,0);
+    vector<int> ans;
+    map<int,vector<int> > mapped;
+    pair<node* , int> p;
+    queue<pair<node* , int> > q;
+    if(root == NULL)
+    {
+        return ans;
+    }
+
+    q.push(make_pair(root,0));
+    while(!q.empty())
+    {
+        pair<node*,int> temp = q.front();
+        node* front = temp.first;
+        int hd = temp.second;
+        q.pop();
+        mapped[hd].push_back(front->data);
+
+        if(front->left)
+        {
+            q.push(make_pair(front->left,hd-1));
+        }
+
+        if(front->right)
+        {
+            q.push(make_pair(front->right,hd+1));
+        }
+    }
+
+    for(auto i : mapped)
+    {
+        for(auto j : i.second)
+        {
+            ans.push_back(j);
+        }
+    }
+    return ans;
 }
 
 
 int main()
 {
-    
+    vector<int> ans;
+    node* root = new node(1);
+    root->left = new node(2);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right = new node(3);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+    root->right->left->right = new node(8);
+    root->right->right->right = new node(9);
+
+    ans = vertical(root);
+
+    for(auto i : ans)
+    {
+        cout<<i<<" ";
+    }
     
     
     return 0;
